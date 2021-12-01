eb02ec876aef654f41a8c16d046b47a64118cb21
Code Sandbox <hello@codesandbox.io>
2021-12-01 10:07:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.97a930f6aed351c41e3f99dd17a708fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dede55d39702e12ed6ac19fc790a6171",
+    "url": "https://haseenaisahibraheem.github.io/csb-clihb/index.html"
+  },
+  {
+    "revision": "99b7ffd2ebd559c8e063",
+    "url": "https://haseenaisahibraheem.github.io/csb-clihb/static/css/main.e54c786b.chunk.css"
+  },
+  {
+    "revision": "43df90f486116f53a982",
+    "url": "https://haseenaisahibraheem.github.io/csb-clihb/static/js/2.6b0b9070.chunk.js"
+  },
+  {
+    "revision": "99b7ffd2ebd559c8e063",
+    "url": "https://haseenaisahibraheem.github.io/csb-clihb/static/js/main.3a8a1406.chunk.js"
+  },
+  {
+    "revision": "05519c40bde7548300cb",
+    "url": "https://haseenaisahibraheem.github.io/csb-clihb/static/js/runtime~main.0eade3ac.js"
+  }
+]);
